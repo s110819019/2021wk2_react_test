6f7565bbe44437156866b39626a1945ff2e5da97
s110819019 <s110819019@stu.ntue.edu.tw>
2021-03-04 23:26:47 +0800

    init and import css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,52 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        <ul>
+          <li><a href="#">選項1</a></li>
+          <li><a href="#">選項2</a></li>
+          <li><a href="#">選項3</a></li>
+          <li><a href="#">選項4</a></li>
+          <li><a href="#">選項5</a></li>
+        </ul>
+      </div>
+      <div className="container">
+        <nav>
+          <ul>
+            <li>
+              <a href="#">
+                <i className="fa fa-home" aria-hidden="true"></i>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-user" aria-hidden="true"></i>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-pencil" aria-hidden="true"></i>
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fa fa-envelope" aria-hidden="true"></i>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+          <section>
+            <article>
+              <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis vitae exercitationem, fuga ut rerum culpa facere quibusdam sint a adipisci.</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A expedita ipsum voluptatibus totam, nisi consequatur, quas quaerat fugit suscipit sit magni atque dignissimos nostrum, mollitia quasi rerum? Enim corrupti voluptatem doloremque nam possimus molestiae excepturi! Iure beatae consectetur amet ullam alias porro distinctio nulla, repellendus nam saepe atque, ut officia velit blanditiis nostrum placeat qui voluptatem voluptatibus temporibus voluptas non eveniet odio. Laboriosam exercitationem error quod totam porro animi repudiandae harum nam sequi assumenda quae praesentium sapiente et architecto consectetur culpa suscipit placeat laudantium distinctio, illum fugiat nihil. Nisi at eaque dolore rerum repudiandae temporibus tenetur, veritatis adipisci recusandae odio!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro harum consequuntur impedit, ducimus est possimus eveniet perspiciatis saepe totam molestiae! Cupiditate quisquam natus explicabo, maxime harum nemo? Impedit quaerat provident dolorum facilis minus suscipit illo cumque aspernatur perspiciatis voluptatum? Ad debitis, veniam sapiente voluptas, animi optio sunt ducimus facere ex doloremque dignissimos in perspiciatis ipsa sed mollitia eos autem ea quod voluptates? Rem perspiciatis tenetur praesentium quas incidunt ratione voluptate cupiditate commodi, culpa dolorum perferendis aliquid quibusdam obcaecati ea nihil hic aspernatur. Similique voluptatibus voluptatem eligendi, maxime fugit sunt omnis nam nihil facilis commodi consequuntur ex quas ut, placeat accusamus odio? Harum esse cum saepe enim itaque deleniti ad, nobis obcaecati ab tempora iusto sequi eius neque voluptas aut nisi. At quibusdam quidem quaerat temporibus reiciendis nobis! Saepe blanditiis sapiente libero asperiores vero. Distinctio excepturi corporis accusantium praesentium sunt, eius temporibus quidem neque velit voluptatum. Optio, laudantium numquam. Hic at repudiandae eligendi impedit molestiae id nisi quisquam ex. Placeat nam eaque itaque tenetur doloribus accusantium, ea dignissimos. Quasi aspernatur nemo commodi nisi, tenetur veritatis dolores libero dicta! Maiores architecto corrupti minima, amet aut enim accusamus saepe distinctio aliquam ipsum omnis magnam perferendis. Consequuntur hic explicabo veritatis officia quaerat earum accusamus assumenda, in deleniti numquam voluptatum soluta totam quis eum. Fugit et quibusdam animi eum esse alias eveniet ad cupiditate cum excepturi. Nesciunt dignissimos ad fugiat illum qui facilis eius itaque, at accusantium molestias sed esse eaque, eos cum ab ullam quibusdam corporis! Ea, possimus. Nemo facere explicabo vitae harum, nesciunt debitis esse sed, amet eos laboriosam aliquid quo repudiandae voluptas obcaecati voluptate iure ea cupiditate, cumque corporis aut dignissimos quis distinctio! Reiciendis odit blanditiis excepturi deleniti nesciunt fugit pariatur earum ex, harum quae, nulla accusantium, ipsa quisquam? Aut, natus, repellat, blanditiis maxime amet saepe at doloremque similique nemo corrupti porro?</p>
+            </article>
+          </section>
+      </div>
     </div>
   );
 }
